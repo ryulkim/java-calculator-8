f2b2c79883430dfd0b346ad96a847c6cf9c3747c
ryulkim <hobby519@naver.com>
2025-10-20 03:19:54 +0900

    test(DelimiterParser): 쉼표(,) 구분자로 입력 시 올바르게 파싱되는지 검증

---
+++ src/test/java/calculator/DelimiterParserTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DelimiterParserTest {
+
+    @Test
+    public void 정상_입력_구분자_쉼표로만_된_경우() {
+        // given
+        String input = "1,2,3";
+
+        // when
+        int[] result = DelimiterParser.getNumbersByDelimiter(input);
+
+        // then
+        Assertions.assertArrayEquals(new int[]{1, 2, 3}, result);
+    }
+}
