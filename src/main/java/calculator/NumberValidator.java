0942b67ff65b495f99ebf9a70e7243864f2644e5
ryulkim <hobby519@naver.com>
2025-10-20 13:08:51 +0900

    feat(NumberValidator): 양수가 아닌 입력값에 대한 예외 처리 추가

--- src/main/java/calculator/NumberValidator.java
+++ src/main/java/calculator/NumberValidator.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 public class NumberValidator {
 
     public static int[] parseInt(String[] inputs) throws IllegalArgumentException {
-        /* 음수 값일 때 등 입력값 예외처리 필요 */
         return Arrays.stream(inputs).mapToInt(NumberValidator::getInteger).toArray();
     }
 
     private static int getInteger(String input) throws IllegalArgumentException {
         try {
-            return Integer.parseInt(input);
+            int parsedInt = Integer.parseInt(input);
+            if (parsedInt <= 0) {
+                throw new IllegalArgumentException("양수가 아닌 입력값이 들어왔습니다.");
+            }
+            return parsedInt;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력값이 잘못되었습니다.");
         }
