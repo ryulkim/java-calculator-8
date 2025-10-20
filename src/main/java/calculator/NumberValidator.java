2435c6e705978d7b02b5503aabc3e535853d2a59
ryulkim <hobby519@naver.com>
2025-10-20 12:45:54 +0900

    feat(NumberValidator): 숫자 이외 문자가 포함된 입력에 대한 예외 검증 로직 추가

--- src/main/java/calculator/NumberValidator.java
+++ src/main/java/calculator/NumberValidator.java
@@ -4,8 +4,16 @@ import java.util.Arrays;
 
 public class NumberValidator {
 
-    public static int[] parseInt(String[] inputs) {
+    public static int[] parseInt(String[] inputs) throws IllegalArgumentException {
         /* 음수 값일 때 등 입력값 예외처리 필요 */
-        return Arrays.stream(inputs).mapToInt(Integer::parseInt).toArray();
+        return Arrays.stream(inputs).mapToInt(NumberValidator::getInteger).toArray();
+    }
+
+    private static int getInteger(String input) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값이 잘못되었습니다.");
+        }
     }
 }
